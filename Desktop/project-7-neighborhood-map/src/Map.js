b1a33a68c03c874535021e1b1bbda9e329023165
Anca Gooje <ancagooje@gmail.com>
2018-10-26 15:04:04 -0400

    added Map.js

---
+++ Desktop/project-7-neighborhood-map/src/Map.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from 'react';
+
+
+export class MapContainer extends Component {
+
+  
+  render() {
+    return(
+      <div>
+        <main>
+
+          <div id="map">
+
+          </div>
+        </main>
+      </div>
+    )
+  }
+
+}
+
+export default MapContainer;
